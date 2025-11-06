1181e04fa748534e24ba8612e9084c6ce5025492
Kira Schauman <kira.schauman@gmail.com>
2025-11-06 14:19:45 +0200

    client profile connected

--- frontend/src/app/components/clientProfile/clientProfile.component.ts
+++ frontend/src/app/components/clientProfile/clientProfile.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-clientProfile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './clientProfile.component.html',
   styleUrl: './clientProfile.component.css'
 })
-export class ClientProfileComponent {
+export class ClientProfileComponent implements OnInit {
+  user: User | null = null;
+  isLoggedIn = false;
 
+  constructor(private authService: AuthService, private userService: UserService) {}
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn?.() ?? false;
+
+    // getting the user details from the backend
+    this.userService.getMe().subscribe({
+      next: (data: User) => {
+        this.user = data;
+      },
+      error: (err: unknown) => console.error(err)
+    });
+  }
 }
