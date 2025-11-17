247b8f47d0434167236cb950450c20b51a89c1b9
Linnie43 <lingab43@gmail.com>
2025-11-17 17:18:45 +0200

    Revert "Merge branch 'main' into linnea"
    This reverts commit fb7de1f1eb183bd11504c20ba5407e2aa12014fa, reversing
    changes made to 7b4d86a23b20bd8ba0eca3f670f7c0d0965d068e.

--- backend/media-service/src/main/java/com/buy01/media/controller/MediaController.java
+++ backend/media-service/src/main/java/com/buy01/media/controller/MediaController.java
@@ -22,6 +22,7 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/media")
+@CrossOrigin(origins = "*")
 public class MediaController {
 
     private final MediaRepository mediaRepository;
