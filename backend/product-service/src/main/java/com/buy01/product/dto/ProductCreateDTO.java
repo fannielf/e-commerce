01c0f1193779b087dbe418d6025e7f41d0f09e6d
fvesanen <fanni.vesanen@gmail.com>
2025-11-21 11:10:39 +0200

    fixing price validation

--- backend/product-service/src/main/java/com/buy01/product/dto/ProductCreateDTO.java
+++ backend/product-service/src/main/java/com/buy01/product/dto/ProductCreateDTO.java
@@ -16,7 +16,7 @@ public class ProductCreateDTO {
     private String description;
 
     @NotNull(message = "Price is required")
-    @Min(value = 1, message = "Price must be at least 1")
+    @Positive(message = "Price must be over 0")
     @Max(value = 100000, message = "Price cannot exceed 100000")
     private Double price;
 
