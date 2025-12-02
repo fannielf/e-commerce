b84ad82154fbc5c08bf7ea2d51dd9b72cddb3493
Maris Karu <maris.karu@gritlab.ax>
2025-12-02 15:41:31 +0200

    some addition to the controller#

--- app/backend/user-service/src/test/java/com/buy01/user/controller/UserControllerTest.java
+++ app/backend/user-service/src/test/java/com/buy01/user/controller/UserControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(UserController.class) //added it for testing the controller with jenkins
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
