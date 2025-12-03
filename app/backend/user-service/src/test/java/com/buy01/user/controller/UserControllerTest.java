fc2e5726c963c33a34335aeaa7452d1b2915c090
Maris Karu <maris.karu@gritlab.ax>
2025-12-03 09:39:24 +0200

    added back the autowired

--- app/backend/user-service/src/test/java/com/buy01/user/controller/UserControllerTest.java
+++ app/backend/user-service/src/test/java/com/buy01/user/controller/UserControllerTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -38,8 +37,7 @@ class UserControllerTest {
     @MockBean
     private UserService userService;
 
-    //@Autowired
-    @MockBean
+    @Autowired
     private UserRepository userRepository;
 
     @MockBean
