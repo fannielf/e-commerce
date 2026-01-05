a3ad06cfeb0a38167104e0bb946ec665d423ba4d
fvesanen <fanni.vesanen@gmail.com>
2026-01-05 15:16:56 +0200

    Product client

---
+++ backend/order-service/src/main/java/com/buy01/order/client/ProductClient.java
@@ -0,0 +1,54 @@
+package com.buy01.order.client;
+
+import com.buy01.order.dto.ProductUpdateDTO;
+import com.buy01.order.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ProductClient {
+
+    private final RestTemplate restTemplate;
+    private final String productServiceBaseUrl;
+    private static final Logger log = LoggerFactory.getLogger(ProductClient.class);
+
+
+    public ProductClient(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+        this.productServiceBaseUrl = "http://product-service:8081/api/products";
+    }
+
+    public ProductUpdateDTO getProductById(String productId) {
+        try {
+            String url = productServiceBaseUrl + "/internal/" + productId;
+            return restTemplate.getForObject(url, ProductUpdateDTO.class);
+
+        } catch (HttpClientErrorException e) {
+            throw new NotFoundException("Product not found with ID: " + productId);
+        }
+    }
+
+    public void updateQuantity(String productId, int quantity) {
+        try {
+            String url = productServiceBaseUrl + "/internal/quantity/" + productId;
+            restTemplate.put(url, quantity);
+        } catch (HttpClientErrorException e) {
+
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new NotFoundException("Product not found with ID: " + productId);
+            }
+
+            if (e.getStatusCode() == HttpStatus.CONFLICT) {
+                log.info("Conflict when updating quantity for productId: {}", productId);
+                //throw new ConflictException("Requested quantity is not available");
+            }
+
+            throw e;
+        }
+    }
+}
