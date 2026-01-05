4a203cb8690cbdb59c9b21cdcfe7049cf8c8f58e
Maris Karu <maris.karu@gritlab.ax>
2026-01-05 13:41:49 +0200

    added a auth to the cart methods

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CartResponseDTO, CartItemRequestDTO, CartItemUpdateDTO } from '../models/cart.model';
 import { BASE_URL } from '../constants/constants';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,25 +12,34 @@ export class CartService {
 
   private apiUrl = `${BASE_URL}/api/cart`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  private getAuthHeaders(): { [header: string]: string } {
+    const token = this.authService.getToken();
+    if (token) {
+      return { 'Authorization': `Bearer ${token}` };
+    } else {
+      return {};
+    }
+  }
 
   addToCart(newItem: CartItemRequestDTO): Observable<CartResponseDTO> {
-    return this.http.post<CartResponseDTO>(this.apiUrl, newItem);
+    return this.http.post<CartResponseDTO>(this.apiUrl, newItem, { headers: this.getAuthHeaders() });
   }
 
   getCart(): Observable<CartResponseDTO> {
-    return this.http.get<CartResponseDTO>(this.apiUrl);
-  }
+      return this.http.get<CartResponseDTO>(this.apiUrl, { headers: this.getAuthHeaders() });
+    }
 
   updateCartItem(productId: string, update: CartItemUpdateDTO): Observable<CartResponseDTO> {
-    return this.http.put<CartResponseDTO>(`${this.apiUrl}/${productId}`, update);
+    return this.http.put<CartResponseDTO>(`${this.apiUrl}/${productId}`, update, { headers: this.getAuthHeaders() });
   }
 
   deleteItem(productId: string): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${productId}`);
+    return this.http.delete<void>(`${this.apiUrl}/${productId}`, { headers: this.getAuthHeaders() });
   }
 
   clearCart(): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/all`);
+    return this.http.delete<void>(`${this.apiUrl}/all`, { headers: this.getAuthHeaders() });
   }
 }
